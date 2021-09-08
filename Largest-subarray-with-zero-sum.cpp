38bd77dcfe5f98824981c83389e869431e1e83f3
visheshvats <51470125+visheshvats@users.noreply.github.com>
2021-09-08 15:05:59 +0530

    Added and Solved Largest Subarray with sum Zero in 450 DSA Solved using Hashing

---
+++ Largest-subarray-with-zero-sum.cpp
@@ -0,0 +1,48 @@
+#// C++ program to find the length of largest subarray
+// with 0 sum
+#include <bits/stdc++.h>
+using namespace std;
+
+// Returns Length of the required subarray
+int maxLen(int arr[], int n)
+{
+    // Map to store the previous sums
+    unordered_map<int, int> presum;
+
+    int sum = 0;
+    int max_len = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        sum = sum + arr[i];
+
+         if (arr[i] == 0 && max_len == 0)
+            max_len = 1;
+        if (sum == 0)
+            max_len = i + 1;
+            
+        if (presum.count(sum))
+        {
+            max_len = max(max_len, i-presum[sum]);
+        }
+        else
+        {
+
+            presum[sum] = i;
+        }
+
+    }
+
+    return max_len;
+}
+
+// Driver Code
+int main()
+{
+    int arr[] = {15, -2, 2, -8, 1, 7, 10, 23};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    cout << "Length of the longest 0 sum subarray is "
+         << maxLen(arr, n);
+
+    return 0;
+}
