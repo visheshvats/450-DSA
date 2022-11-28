355c4365a2c5b80069177e7b7c3900ebc4494369
visheshvats <51470125+visheshvats@users.noreply.github.com>
2022-11-29 02:15:54 +0530

    RottenOrange.cpp

--- RottenOrange.cpp
+++ RottenOrange.cpp
@@ -1,34 +1,66 @@
-//{ Driver Code Starts
 #include<bits/stdc++.h>
 using namespace std;
 
-// } Driver Code Ends
-class Solution 
-{
-    public:
-    //Function to find minimum time required to rot all oranges. 
-    int orangesRotting(vector<vector<int>>& grid) {
-        // Code here
-    }
-};
 
-//{ Driver Code Starts.
-int main(){
-	int tc;
-	cin >> tc;
-	while(tc--){
-		int n, m;
-		cin >> n >> m;
-		vector<vector<int>>grid(n, vector<int>(m, -1));
-		for(int i = 0; i < n; i++){
-			for(int j = 0; j < m; j++){
-				cin >> grid[i][j];
+class Solution {
+public:
+    int orangesRotting(vector<vector<int>>& grid) {
+
+		int n= grid.size();
+		int m = grid[0].size();
+
+		vector<vector<int>> vis(n, vector<int>(m,0));
+		queue<pair<pair<int, int>, int>> q; //{{r,c},t}
+		for(int i=0;i<n;i++)
+		{
+			for(int j=0;j<m;j++)
+			{
+				if(grid[i][j]==2)
+				{
+					q.push({{i,j},0});
+				}
 			}
 		}
-		Solution obj;
-		int ans = obj.orangesRotting(grid);
-		cout << ans << "\n";
-	}
-	return 0;
-}
-// } Driver Code Ends
+
+		int tm =0;
+
+		vector<int> delrow = {1,0,-1,0};
+		vector<int> delcol = {0,1,0,-1};
+		while(!q.empty())
+		{
+			int r = q.front().first.first;
+			int c = q.front().first.second;
+			int t = q.front().second;
+			q.pop();
+			tm= max(tm,t);
+
+			for(int i=0;i<4;i++)
+			{
+				int nrow = r+delrow[i];
+				int ncol = c+delcol[i];
+				if(nrow>=0&&ncol>=0&&nrow<n&&ncol<m&&vis[nrow][ncol]!=2&&grid[nrow][ncol]==1)
+				{
+					q.push({{nrow, ncol},t+1});
+					vis[nrow][ncol]=2;
+				}
+			}
+
+
+		}
+
+		for(int i=0;i<n;i++)
+		{
+			for(int j=0;j<m;j++)
+			{
+				if(vis[i][j]==0&&grid[i][j]==1)
+					return -1;
+			}
+		}
+
+
+
+		return tm;
+
+        
+    }
+};
